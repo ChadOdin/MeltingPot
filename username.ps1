23c3067fafa1a730ebf0129f8b10081bc83f21a9
ChadOdin <141269054+ChadOdin@users.noreply.github.com>
2024-02-27 16:06:10 +0000

    exchangeAIO.ps1

--- username.ps1
+++ username.ps1
@@ -1,3 +1,27 @@
+# Function to add or remove users from distribution group
+function ManageDistributionGroup {
+    param (
+        [string]$action,
+        [string]$userUPN
+    )
+
+    switch ($action) {
+        'Add' {
+            Add-DistributionGroupMember -Identity $distributionGroupName -Member $userUPN
+            Write-Host "Added $userUPN to $distributionGroupName."
+            Log-Action "Added $userUPN to $distributionGroupName."
+        }
+        'Remove' {
+            Remove-DistributionGroupMember -Identity $distributionGroupName -Member $userUPN -Confirm:$false
+            Write-Host "Removed $userUPN from $distributionGroupName."
+            Log-Action "Removed $userUPN from $distributionGroupName."
+        }
+        default {
+            Write-Host "Invalid choice. Please enter 'Add' or 'Remove'."
+        }
+    }
+}
+
 # Connect to Exchange server
 $exchangeCredential = Get-Credential -Message "Enter your Exchange admin credentials"
 $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://$exchangeServer/PowerShell/ -Authentication Kerberos -Credential $exchangeCredential
@@ -15,78 +39,63 @@ Write-Host "Exchange Toolset" -ForegroundColor Yellow
 # Main menu to choose between adding or removing users from distribution groups
 $mainChoice = Read-Host "`nSelect an option:`n1. Adding users to distribution groups`n2. Removing users from distribution groups`nEnter 1 or 2"
 
-if ($mainChoice -eq 1) {
-    # Adding users to distribution groups
-    # Submenu to choose between adding a single user and adding users from CSV
-    $submenuChoice = Read-Host "`nSelect an option:`n1. Add single user to distribution group`n2. Add users from CSV to distribution group`nEnter 1 or 2"
+switch ($mainChoice) {
+    1 {
+        # Adding users to distribution groups
+        # Submenu to choose between adding a single user and adding users from CSV
+        $submenuChoice = Read-Host "`nSelect an option:`n1. Add single user to distribution group`n2. Add users from CSV to distribution group`nEnter 1 or 2"
 
-    if ($submenuChoice -eq 1) {
-        # Adding a single user to distribution group
-        $userUPN = Read-Host "Enter user UPN"
-        Add-DistributionGroupMember -Identity $distributionGroupName -Member $userUPN
-        Write-Host "Added $userUPN to $distributionGroupName."
-        Log-Action "Added $userUPN to $distributionGroupName."
-    }
-    elseif ($submenuChoice -eq 2) {
-        # Adding users from CSV to distribution group
-        # Path to the CSV file with UPNs
-        $csvPath = Read-Host "Enter the path to the CSV file"
+        switch ($submenuChoice) {
+            1 {
+                # Adding a single user to distribution group
+                $userUPN = Read-Host "Enter user UPN"
+                ManageDistributionGroup -action 'Add' -userUPN $userUPN
+            }
+            2 {
+                # Adding users from CSV to distribution group
+                $csvPath = Read-Host "Enter the path to the CSV file"
+                $userList = Import-Csv $csvPath
 
-        # Import CSV file
-        $userList = Import-Csv $csvPath
-
-        # Iterate through each user in the CSV file and add them to the distribution group
-        foreach ($user in $userList) {
-            $userUPN = $user.UPN
-            Add-DistributionGroupMember -Identity $distributionGroupName -Member $userUPN
-            Write-Host "Added $userUPN to $distributionGroupName."
-            Log-Action "Added $userUPN to $distributionGroupName."
+                foreach ($user in $userList) {
+                    ManageDistributionGroup -action 'Add' -userUPN $user.UPN
+                }
+            }
+            default {
+                Write-Host "Invalid choice. Please enter 1 or 2."
+            }
         }
     }
-    else {
-        Write-Host "Invalid choice. Please enter 1 or 2."
-    }
-}
-elseif ($mainChoice -eq 2) {
-    # Removing users from distribution groups
-    # Submenu to choose between removing a single user and removing users from CSV
-    $submenuChoice = Read-Host "`nSelect an option:`n1. Remove single user from distribution group`n2. Remove users from CSV from distribution group`nEnter 1 or 2"
+    2 {
+        # Removing users from distribution groups
+        $submenuChoice = Read-Host "`nSelect an option:`n1. Remove single user from distribution group`n2. Remove users from CSV from distribution group`nEnter 1 or 2"
 
-    if ($submenuChoice -eq 1) {
-        # Removing a single user from distribution group
-        $userUPN = Read-Host "Enter user UPN"
-        Remove-DistributionGroupMember -Identity $distributionGroupName -Member $userUPN -Confirm:$false
-        Write-Host "Removed $userUPN from $distributionGroupName."
-        Log-Action "Removed $userUPN from $distributionGroupName."
-    }
-    elseif ($submenuChoice -eq 2) {
-        # Removing users from CSV from distribution group
-        # Path to the CSV file with UPNs
-        $csvPath = Read-Host "Enter the path to the CSV file"
+        switch ($submenuChoice) {
+            1 {
+                # Removing a single user from distribution group
+                $userUPN = Read-Host "Enter user UPN"
+                ManageDistributionGroup -action 'Remove' -userUPN $userUPN
+            }
+            2 {
+                # Removing users from CSV from distribution group
+                $csvPath = Read-Host "Enter the path to the CSV file"
+                $userList = Import-Csv $csvPath
 
-        # Import CSV file
-        $userList = Import-Csv $csvPath
-
-        # Iterate through each user in the CSV file and remove them from the distribution group
-        foreach ($user in $userList) {
-            $userUPN = $user.UPN
-            Remove-DistributionGroupMember -Identity $distributionGroupName -Member $userUPN -Confirm:$false
-            Write-Host "Removed $userUPN from $distributionGroupName."
-            Log-Action "Removed $userUPN from $distributionGroupName."
+                foreach ($user in $userList) {
+                    ManageDistributionGroup -action 'Remove' -userUPN $user.UPN
+                }
+            }
+            default {
+                Write-Host "Invalid choice. Please enter 1 or 2."
+            }
         }
     }
-    else {
+    default {
         Write-Host "Invalid choice. Please enter 1 or 2."
     }
-}
-else {
-    Write-Host "Invalid choice. Please enter 1 or 2."
 }
 
 # Disconnect from the Exchange server
 Remove-PSSession $session
 
-
 Clear-Host
-Write-Host "script exited successfully. All user actions have been logged for
-auditing purposes."
+Write-Host "Script exited successfully. All user actions have been logged for auditing purposes."
