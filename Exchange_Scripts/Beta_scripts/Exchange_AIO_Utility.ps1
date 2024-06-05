843e74563bce6a043aa8bd8f88dffc60843894a7
ChadOdin <141269054+ChadOdin@users.noreply.github.com>
2024-06-05 07:06:00 +0100

    test

--- Exchange_Scripts/Beta_scripts/Exchange_AIO_Utility.ps1
+++ Exchange_Scripts/Beta_scripts/Exchange_AIO_Utility.ps1
@@ -1,5 +1,21 @@
 function Show-Menu {
     Clear-Host
+<<<<<<< HEAD
+    Write-Host "Select a menu option to run`n
+1: Bulk Addition to Distribution Group`n
+2: Bulk Removal from Distribution Group`n
+3: Singular Addition to Distribution Group`n
+4: Singular Removal from Distribtution Group`n
+5: Mailbox Creation`n
+6: Bulk Addition to Calendar`n
+7: Bulk Removal from Calendar`n
+8: Single Addition to Calendar`n
+9: Single Removal from calendar`n
+10: Bulk Addition to Mailbox`n
+11: Bulk Removal from Mailbox
+12: Exit
+"
+=======
     Write-Host "Select a menu option to run`n"
     Write-Host "1: Bulk Addition to Distribution Group`n"
     Write-Host "2: Bulk Removal from Distribution Group`n"
@@ -12,7 +28,10 @@ function Show-Menu {
     Write-Host "9: Single Removal from Calendar`n"
     Write-Host "10: Exit`n"
 }
+>>>>>>> 076a487b324c8fa762a8f3921e61a982d12ba36e
 
+<<<<<<< HEAD
+=======
 function Connect-Exchange {
     if (-not (Get-Module -ListAvailable -Name ExchangeOnlineManagement)) {
         Install-Module -Name ExchangeOnlineManagement -Scope AllUsers -Force
@@ -33,6 +52,7 @@ function Get-MailboxLocation($mailbox) {
     } catch {
         throw "Error fetching mailbox location for $mailbox: $_"
     }
+>>>>>>> 076a487b324c8fa762a8f3921e61a982d12ba36e
 }
 
 do {
@@ -41,6 +61,7 @@ do {
 
     switch ($choice) {
         1 {
+            Clear-Host
             Write-Host "Bulk Addition to Distribution Group Selected"
             $Group = Read-Host -Prompt "Please specify group"
             $csv = Read-Host -Prompt "Please specify path for CSV file"
@@ -48,13 +69,19 @@ do {
             Import-CSV $csv | ForEach-Object {Add-DistributionGroupMember -Identity $Group -Member $_.UPN}
         }
         2 {
+<<<<<<< HEAD
+            Clear-Host
+            Write-Host "Bulk Removal to Distribution Group Selected"
+=======
             Write-Host "Bulk Removal from Distribution Group Selected"
+>>>>>>> 076a487b324c8fa762a8f3921e61a982d12ba36e
             $Group = Read-Host -Prompt "Please specify group"
             $csv = Read-Host -Prompt "Please specify path for CSV file"
             Connect-Exchange
             Import-CSV $csv | ForEach-Object {Remove-DistributionGroupMember -Identity $Group -Member $_.UPN}
         }
         3 {
+            Clear-Host
             Write-Host "Singular Addition to Distribution Group"
             $user = Read-Host -Prompt "Please Specify User's Email"
             $Group = Read-Host -Prompt "Please specify the Distribution Group"
@@ -62,19 +89,38 @@ do {
             Add-DistributionGroupMember -Identity $Group -Member $user
         }
         4 {
+<<<<<<< HEAD
+            Clear-Host
+            Write-Host "Singular Addition to Distribution Group"
+            $user = Read-Host -Prompt "Please Specify Users Email I.E (First.Last@placesforpeople.co.uk NOT payroll)"
+=======
             Write-Host "Singular Removal from Distribution Group"
             $user = Read-Host -Prompt "Please Specify User's Email"
+>>>>>>> 076a487b324c8fa762a8f3921e61a982d12ba36e
             $Group = Read-Host -Prompt "Please specify the Distribution Group"
             Connect-Exchange
             Remove-DistributionGroupMember -Identity $Group -Member $user
         }
+<<<<<<< HEAD
+        
+        5{
+            Clear-Host
+            Write-Host "Mailbox Creation"
+=======
         5 {
+>>>>>>> 076a487b324c8fa762a8f3921e61a982d12ba36e
             $alias = Read-Host -Prompt "Enter Mailbox Alias"
             $name = Read-Host -Prompt "Enter Mailbox Name"
             $First = Read-Host -Prompt "Enter First Name"
             $Last = Read-Host -Prompt "Enter Last Name"
+<<<<<<< HEAD
+            $SAM = Read-Host -Prompt "Enter SAM. (This counts as a payroll so make sure it matches the mailbox in some capacity I.E SrcSpt")
+            $UPN = Read-Host -Prompt "Enter UPN `n This Script automatically appends the full email address so only enter the desired display name for the email address"
+
+=======
             $SAM = Read-Host -Prompt "Enter SAM"
             $UPN = Read-Host -Prompt "Enter UPN (This script automatically appends the full email address so only enter the desired display name for the email address)"
+>>>>>>> 076a487b324c8fa762a8f3921e61a982d12ba36e
             $credentials = Get-Credential
             $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://server/Powershell/ -Authentication Kerberos -Credential $credentials
             Import-PSSession $Session
@@ -86,7 +132,16 @@ do {
             $csvpath = Read-Host -Prompt "Enter CSV Path"
             $accessList = Import-Csv -Path $csvpath
 
+<<<<<<< HEAD
+        6{
+            Clear-Host
+            Write-Host "Bulk Addition to Calendar"
+            $Credentials = Get-Credential
+            $Session = New-PSSession -ConfiguartionName Microsoft.Exchange -ConnectionUri http://mbx900004.group.net/Powershell/ -Authentication Kerberos -Credential $credentials
+            Import-PSSession $Session
+=======
             Connect-Exchange
+>>>>>>> 076a487b324c8fa762a8f3921e61a982d12ba36e
 
             $location = Get-MailboxLocation $UserMailbox
 
@@ -108,9 +163,20 @@ do {
             }
         }
         7 {
+<<<<<<< HEAD
+            Clear-Host
+            Write-Host "Bulk Removal from Calendar"
+            $Credentials = Get-Credential
+            $Session = New-PSSession -ConfiguartionName Microsoft.Exchange -ConnectionUri http://mbx900004.group.net/Powershell/ -Authentication Kerberos -Credential $credentials
+            Import-PSSession $Session
+
+            $UserMailbox = Read-host -Prompt "Enter Mailbox"
+            $csvpath = Read-host -Prompt "Enter CSV Path"
+=======
             Write-Host "Bulk Removal from Calendar Selected"
             $UserMailbox = Read-Host -Prompt "Enter Mailbox"
             $csvpath = Read-Host -Prompt "Enter CSV Path"
+>>>>>>> 076a487b324c8fa762a8f3921e61a982d12ba36e
             $accessList = Import-Csv -Path $csvpath
 
             Connect-Exchange
@@ -133,7 +199,15 @@ do {
             }
         }
         8 {
+<<<<<<< HEAD
+            Clear-Host
+            Write-Host "Single Addition to calendar"
+            $Credentials = Get-Credential
+            $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://mbx900004/Powershell/ -Authentication Kerberos -Credential $Credentials
+
+=======
             Write-Host "Single Addition to Calendar Selected"
+>>>>>>> 076a487b324c8fa762a8f3921e61a982d12ba36e
             $UserMailbox = Read-Host -Prompt "Enter Mailbox"
             $useradd = Read-Host -Prompt "Enter User's UPN"
 
@@ -160,6 +234,20 @@ do {
             $UserMailbox = Read-Host -Prompt "Enter Mailbox"
             $userremove = Read-Host -Prompt "Enter User's UPN"
 
+<<<<<<< HEAD
+        9{
+            Clear-Host
+            Write-Host "Single Removal from Calendar"
+
+            $Credentials = Get-Credential
+            $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://mbx900004/Powershell/ -Authentication Kerberos -Credential $Credentials
+
+            $UserMailbox = Read-Host -Prompt "Enter Mailbox"
+            $useradd = Read0Host -Prompt "Enter User's UPN"
+
+            Add-MailboxFolderPermission -Identity "$UserMailbx:\Calendar" -User $useradd -AccessRights Reviewer
+            Set-MailboxFolderPermission -Identity "$userMailbox:\Calendar" -User $useradd -AccessRights Reviewer
+=======
             Connect-Exchange
 
             $location = Get-MailboxLocation $UserMailbox
@@ -175,13 +263,20 @@ do {
             } catch {
                 Write-Error "Failed to remove permissions for $userremove on $UserMailbox"
             }
+>>>>>>> 076a487b324c8fa762a8f3921e61a982d12ba36e
         }
+<<<<<<< HEAD
+
+        }
+        default {write-host "invalid Choice"
+=======
         10 {
             Write-Host "Exiting..."
         }
         default {
             Write-Host "Invalid Choice"
         }
+>>>>>>> 076a487b324c8fa762a8f3921e61a982d12ba36e
     }
 } until ($choice -eq 10)
 
